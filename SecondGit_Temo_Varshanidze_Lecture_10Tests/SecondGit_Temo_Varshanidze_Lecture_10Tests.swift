92eadcfe2dcb75e25dfd780321c3312715632e13
Temo Varshanidze <anarogava@anas-MacBook-Air.local>
2025-02-19 15:43:02 +0400

    MySecProject

---
+++ SecondGit_Temo_Varshanidze_Lecture_10Tests/SecondGit_Temo_Varshanidze_Lecture_10Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SecondGit_Temo_Varshanidze_Lecture_10Tests.swift
+//  SecondGit_Temo_Varshanidze_Lecture_10Tests
+//
+//  Created by ana rogava on 19.02.25.
+//
+
+import Testing
+@testable import SecondGit_Temo_Varshanidze_Lecture_10
+
+struct SecondGit_Temo_Varshanidze_Lecture_10Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
